13bb70bff3cc5837062e3f3b4cdad7f1620dda01
Igor Libar <igor10212005@gmail.com>
2025-09-19 09:14:27 +0300

    Добавлен полностью рабочий header + набросок на footer

--- scripts/burger.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const burger = document.querySelector(".burger");
-  const menu = document.querySelector("header nav ul");
-
-  burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-    menu.classList.toggle("active");
-  });
-});
